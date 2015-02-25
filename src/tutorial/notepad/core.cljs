00341b3334b8e997e5d7629fac1fb5f3b937d453
Larry Jones <mrwizard82d1@gmail.com>
2015-02-25 07:26:00 -0600

    Finally! Some output.

--- src/tutorial/notepad/core.cljs
+++ src/tutorial/notepad/core.cljs
@@ -1,5 +1,6 @@
 (ns ^:figwheel-always tutorial.notepad.core
-    (:require))
+    (:require [goog.dom :as dom]
+              [goog.ui.Zippy :as zippy]))
 
 (enable-console-print!)
 
@@ -9,4 +10,33 @@
 
 (defonce app-state (atom {:text "Hello world!"}))
 
+;; Makes a DOM element for a single note.
+(defn make-note-dom [note]
+  (let [header-element
+        (dom/createDom "div"
+                       #js {:style #js {:backgroundColor "#EEE"}}
+                       (.-title note))
+        content-element (dom/createDom "div" nil (.-content note))
+        new-note-dom (dom/createDom "div" nil
+                                    header-element content-element)]
+    (dom/appendChild (.-parent note) new-note-dom)
+    (zippy/Zippy. header-element content-element)))
 
+
+;;"Main" function for page.
+(defn main []
+  (let [notes-container (.getElementById js/document "notes")
+        note-titles ["Note 1" "Note2"]
+        note-contents ["Content of Note 1" "Content of Note 2"]
+        notes (map #(clj->js {:title %1 :content %2 :parent %3})
+                   note-titles note-contents (cycle [notes-container]))]
+    (println (js/Date.))
+    (println (map
+              ;; (fn [t c p] #js {:title t :content c :parent p})
+              #(clj->js {:title %1 :content %2 :parent %3})
+                  note-titles note-contents (cycle [notes-container])))
+    (doseq [note notes]
+      (make-note-dom note))))
+
+;; Invoke the main function.
+(main)
